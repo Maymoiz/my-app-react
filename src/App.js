8318a809573963c3a368228332440d19c02557ac
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-10-15 12:44:24 +0200

    Injected bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
      <h1>React Weather App</h1>
+     <footer>This project was created by <a href="https://github.com/Maymoiz" target="_blank" rel="noopener noreferrer">Moisha💕</a>. It is open-sourced on <a href="https://github.com/Maymoiz/my-app-react" target="_blank" rel="noopener noreferrer">Github</a> and hosted on <a href="https://mo-react-weather-app.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.</footer>
+     </div>
     </div>
   );
 }
