f82578b19febfe3a6f1db30bc594727a3dfe6a4b
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-10-15 13:12:11 +0200

    Added html and the weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-     <h1>React Weather App</h1>
+     <h1 className="text-center">React Weather App</h1>
+     <Weather />
      <footer>This project was created by <a href="https://github.com/Maymoiz" target="_blank" rel="noopener noreferrer">Moisha💕</a>. It is open-sourced on <a href="https://github.com/Maymoiz/my-app-react" target="_blank" rel="noopener noreferrer">Github</a> and hosted on <a href="https://mo-react-weather-app.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.</footer>
      </div>
     </div>
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import './Weather.css';
+
+export default function Weather() {
+    return (
+        <div className="Weather">
+            <form>
+                <div className="row">
+                    <div className="col-9">
+                        <input
+                            type="search"
+                            placeholder="Enter a city..."
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="col-3">
+                        <button type="submit" className="btn btn-primary w-100">
+                            Search
+                        </button>
+                    </div>
+                </div>
+            </form>
+            <h1>Pretoria</h1>
+            <ul>
+                <li>Wednesday 13:00</li>
+                <li>Sunny</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                    <img src="https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v10/Condition_Card/SunnyDayV3.svg"
+                        alt="Sunny" />
+                    <span className="temperature">25</span>
+                    <span className="units">°C | °F</span>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Humidity: 60%</li>
+                        <li>Wind: 10 km/h</li>
+                        <li>Precipitation: 0%</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+  }
