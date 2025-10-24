418d49f95af62052d056208a5995f12127f15e40
Maymoiz <208803754+Maymoiz@users.noreply.github.com>
2025-10-24 02:11:57 +0200

    rmoved a container

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">
+      
      <h1 className="text-center">React Weather App</h1>
      <Weather defaultCity="Tokyo" />
-     <footer>This project was created by <a href="https://github.com/Maymoiz" target="_blank" rel="noopener noreferrer">Moisha💕</a>. It is open-sourced on <a href="https://github.com/Maymoiz/my-app-react" target="_blank" rel="noopener noreferrer">Github</a> and hosted on <a href="https://mo-react-weather-app.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.</footer>
-     </div>
+     <footer>This project was created by <a href="https://github.com/Maymoiz" target="_blank" rel="noopener noreferrer">Moisha💕</a>. It is open-sourced on <a href="https://github.com/Maymoiz/my-app-react" target="_blank" rel="noopener noreferrer">Github</a> and hosted on <a href="https://mo-react-weather-app.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a>.</footer>  
     </div>
   );
 }
